ba80a23230c81f8d63fe30450cc1afb087851705
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-03-03 23:05:34 +0200

    28

--- bt/urls.py
+++ bt/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('brand.urls')),
 ]
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
