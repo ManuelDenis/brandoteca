4e41bad822534615b2873820f36563f8566493ab
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-03-03 22:58:42 +0200

    25

--- bt/settings.py
+++ bt/settings.py
@@ -25,9 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://brandoteca.herokuapp.com/', '127.0.0.1:8000']
+
 
 # Application definition
 
