43d2111e3d930f1202294b3f41d554feb05ee5ca
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-03-14 23:45:18 +0200

    j70u

--- brand/forms.py
+++ brand/forms.py
@@ -1,21 +1,7 @@
-from allauth.account.forms import SignupForm
 from django import forms
-
 import DataChoices.country
 from brand.models import Newsletter, Brand
 from bt import settings
-
-
-class CustomSignupForm(SignupForm):
-    first_name = forms.CharField(max_length=30, label='First Name')
-    last_name = forms.CharField(max_length=30, label='Last Name')
-
-    def save(self, request):
-        user = super(CustomSignupForm, self).save(request)
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
-        return user
 
 
 class NewsletterForm(forms.ModelForm):
