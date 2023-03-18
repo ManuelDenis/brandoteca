663d018005d3e9b125b44f3463631e233da269cc
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-03-18 11:12:55 +0200

    8opf

--- bt/settings.py
+++ bt/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bt.wsgi.application'
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,7 +89,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
